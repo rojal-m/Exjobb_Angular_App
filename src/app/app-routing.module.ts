44b00255e695b7dc9f987cbc89bc0a0e5261abe8
roro977 <135514313+roro977@users.noreply.github.com>
2023-11-14 14:10:36 +0100

    navigating between different pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
+import { OverviewComponent } from './overview/overview.component';
+import { FormComponent } from './form/form.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-signup', component: LoginSignupComponent },
+  { path: 'overview', component: OverviewComponent, canActivate: [AuthGuard] },
+  { path: 'form', component: FormComponent, canActivate: [AuthGuard] },
+  { path: 'form/:id', component: FormComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/login-signup', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
